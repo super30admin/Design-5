e89a44c108cfa8dda76a3818eb66371f80bac06a
Rishi Raj <rishiraj824@gmail.com>
2021-01-19 21:41:28 -0500

    Update copy-list-with-random.java

--- copy-list-with-random.java
+++ copy-list-with-random.java
@@ -1,4 +1,46 @@
 // Time - O(N)
+// Space - O(1)
+
+class Solution {
+    public Node copyRandomList(Node head) {
+        if(head == null) {
+            return head;
+        }
+        
+        Node curr = head;
+        
+        // create new list
+        while(curr != null) {
+            Node temp = curr.next;
+            curr.next = new Node(curr.val);
+            curr.next.next = temp;
+            curr = curr.next.next;
+        }
+        curr = head;
+        // deal with random
+        while(curr !=null) {
+            if(curr.random!=null){
+                curr.next.random = curr.random.next;                
+            }
+            curr = curr.next.next;
+        }
+        // break the list
+        curr = head;
+        Node newList = curr.next;
+        Node dummy = newList;
+        while(curr!=null) {
+            curr.next = curr.next.next;
+            if(newList.next!=null) {
+                newList.next = newList.next.next;
+            }
+            curr = curr.next;
+            newList = newList.next;
+        }
+        return dummy;
+    }
+}
+
+// Time - O(N)
 // Space - O(N)
 class Solution {
     HashMap<Node,Node> map;
