02472682c178198e23c7aa0d385371011f66ed62
Nirbhay Sibal <sibal.nirbhaytlc@gmail.com>
2021-02-17 21:10:14 -0600

    Completed Exercise

---
+++ CopyListWithRandomPtr.py
@@ -0,0 +1,28 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+    def __init__(self):
+        self.hash_map = {}
+
+    def copyRandomList(self, head: 'Node') -> 'Node':
+        return self.deep_copy(head)
+
+    def deep_copy(self, node):
+        if node is None:
+            return None
+
+        if node in self.hash_map:
+            return self.hash_map[node]
+
+        n = Node(node.val)
+        self.hash_map[node] = n
+        n.next = self.deep_copy(node.next)
+        n.random = self.deep_copy(node.random)
+        return n
