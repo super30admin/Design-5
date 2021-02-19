7dbffa5fdae54bf8b98d4aba1ee02874a0aeddc1
Sachin Devatar <73754947+SachinDevatar77@users.noreply.github.com>
2021-02-18 21:13:48 -0600

    Update Parking_Lot.py

--- Parking_Lot.py
+++ Parking_Lot.py
@@ -6,83 +6,73 @@ Your code here along with comments explaining your approach:
 We use min heap here for storing all the available parking spots and use a comparator to pop out
 the minimum one.
 """
-from heapq import heappush as insert
-from heapq import heappop as remove
-from random import randint
 
+import heapq
 
 class ParkingSpot:
-    def __init__(self, floor: int, spot: int):
+    def __init__(self, floor, spot):
         self.floor = floor
         self.spot = spot
 
-    def getFloor(self):
-        return self.floor
-
-    def getSpot(self):
-        return self.spot
-
     def __lt__(self, other):
-        if self.getFloor() == other.getFloor():
-            return (self.getSpot() < other.getSpot())
-        return (self.getFloor() < other.getFloor())
+        if self.floor == other.floor:
+            return self.spot < other.spot
+        return self.floor < other.floor
 
-    def printDetails(self):
-        return 'Floor: {} || Slot: {}'.format(self.getFloor(), self.getSpot())
-
+    def __str__(self):
+        return 'Parked at Floor: {}, Spot: {}'.format(self.floor, self.spot)
 
 class ParkingLot:
-    def __init__(self, maxFloors, spotsPerFloor):
-        self.maxFloors = maxFloors
-        self.spotsPerFloor = spotsPerFloor
-        self.heap = []
-        for i in range(self.maxFloors):
-            for j in range(self.spotsPerFloor):
-                self.addSpot(i, j)
+    def __init__(self, max_floors, spots_per_floor):
+        self.max_floors = max_floors
+        self.spots_per_floor = spots_per_floor
+        self.available_spots = []
 
-    def park(self):
-        if len(self.heap) > 0:
-            return remove(self.heap)
-        else:
-            print("Parking lot full.")
-            return None
+    def _is_full(self):
+        return len(self.available_spots) == 0
 
-    def unpark(self, ParkingSpot):
+    def park(self):                                     # O(1)
+        if self._is_full():
+            raise Exception('Parking Lot is full')
 
-        floor = ParkingSpot.getFloor()
-        spot = ParkingSpot.getSpot()
+        return heapq.heappop(self.available_spots)
 
-        if (floor < self.maxFloors and spot < self.spotsPerFloor):
-            self.addSpot(floor, spot)
+    def get_next_available(self):                       # O(1)
+        if self._is_full():
+            raise Exception('Parking Lot is full')
 
-    def addSpot(self, floor, spot):
-        if floor > self.maxFloors or spot > self.spotsPerFloor:
-            print("Value out of bounds")
-        else:
-            newSpot = ParkingSpot(floor, spot)
-            insert(self.heap, newSpot)
+        return self.available_spots[0]
 
-    def getNextAvailableSpot(self):
-        return self.heap[0]
+    def unpark(self, floor, spot):                      # O(logn)
+        self._add_vacant_spot(floor, spot)
 
-    def status(self):
-        return 'Available Slots: {}'.format(len(self.heap))
+    def add_parking_spot(self, floor, spot):            # O(logn)
+        if floor > self.max_floors or spot > self.spots_per_floor:
+            raise Exception('Input is out of allowed range')
+
+        self._add_vacant_spot(floor, spot)
+
+    def _add_vacant_spot(self, floor, spot):
+        parking_spot = ParkingSpot(floor, spot)
+        heapq.heappush(self.available_spots, parking_spot)
 
 
 def main():
+    pl = ParkingLot(10, 20)
 
-    parkingLot = ParkingLot(2, 2)
+    pl.add_parking_spot(1, 1)
+    pl.add_parking_spot(2, 1)
+    pl.add_parking_spot(3, 1)
+    pl.add_parking_spot(1, 2)
+    pl.add_parking_spot(2, 2)
+    pl.add_parking_spot(3, 2)
 
-    for i in range(5):
-        print(parkingLot.status())
-        print("Parking: {}".format(parkingLot.park()))
+    print(pl.get_next_available())
 
-    for i in range(3):
-        parkingSpot = ParkingSpot(randint(0, 2), randint(0, 3))
-        parkingLot.unpark(parkingSpot)
-        print(parkingLot.status())
-        print("Parking: {}".format(parkingLot.park()))
+    pl.park()
+    print(pl.get_next_available())
 
+    pl.unpark(1, 1)
+    print(pl.get_next_available())
 
-if __name__ == '__main__':
-    main()
+main()
