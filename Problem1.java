24b418dd335133bce7033bccb6f50e0b5d2d27d1
Debdyuti Das <debdyuti@Debdyutis-MacBook-Air.local>
2023-08-08 13:22:22 -0700

    Added design 5

---
+++ Problem1.java
@@ -0,0 +1,49 @@
+ //TC = O(3n)
+ // SC = O(1)
+
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+public class RandomPtr {
+   
+    public Node copyRandomList(Node head) {
+        if(head==null)return null;
+        //Step1
+        Node curr = head;
+        while(curr!=null){
+            Node copyNode = new Node(curr.val);
+            copyNode.next = curr.next;
+            curr.next = copyNode;
+            curr = curr.next.next;
+        }
+      
+        curr = head;
+        while(curr!=null){
+            if(curr.random!=null)
+                curr.next.random = curr.random.next;
+            curr = curr.next.next;
+        }
+       
+        curr = head;
+        Node newHead = head.next;
+        Node copyCurr = newHead;
+        while(curr!=null){
+            curr.next = curr.next.next;
+            if(copyCurr.next!=null)
+                copyCurr.next = copyCurr.next.next;
+            curr = curr.next;
+            copyCurr = copyCurr.next;
+        }
+        return newHead;
+
+    }
+}
+
