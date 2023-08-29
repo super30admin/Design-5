3325d4976b705453ffcf009bc191033240e29040
NisargaVenkatesh <venkatesh.ni@northeastern.edu>
2023-08-29 17:38:51 -0400

    Exercise for review

---
+++ randompointer.py
@@ -0,0 +1,32 @@
+#time: O(n)
+#space:O(n)
+class Solution:
+    def copyRandomList(self, head: 'Node') -> 'Node':
+        
+        if not head:
+            return head
+        
+        old_node = head
+        
+        while(old_node):
+            
+            temp= old_node.next
+            new_node = Node(old_node.val,None,None)
+            old_node.next = new_node
+            new_node.next = temp
+            old_node = new_node.next
+        pointer = head
+        old_node = head
+        while(old_node and old_node.next):
+            if old_node.random and old_node.next:
+                old_node.next.random = old_node.random.next
+            old_node = old_node.next.next
+        old_node = head
+        new_head = old_node.next
+        new_node = old_node.next
+        while(old_node and old_node.next and new_node and new_node.next ):
+            old_node.next = old_node.next.next
+            new_node.next = new_node.next.next
+            old_node = old_node.next
+            new_node = new_node.next
+        return new_head
