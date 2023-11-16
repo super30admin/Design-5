fd6a1ff2b7628b3602b4f400e55a52447f189c60
Sagar Raval <62638370+SRaval20@users.noreply.github.com>
2023-11-16 14:35:05 -0700

    Update CopyListWithRandomPointer.java

--- src/CopyListWithRandomPointer.java
+++ src/CopyListWithRandomPointer.java
@@ -13,96 +13,160 @@ class Node {
 }
 */
 
+
+// ******************** Modifying given linked list, without extra space ********************
+
 // Time Complexity:  O(n)
-// Space Complexity: O(n)
+// Space Complexity: O(1)
 
-
-
-// ******************** Iterating once ********************
 
 class Solution {
 
     Map<Node, Node> oldToNewNodes;
     
     public Node copyRandomList(Node head) {
-        
-        Node dummyHead = new Node(-1);
-        oldToNewNodes = new HashMap<>();            // creating map from old node to new node
 
-        Node curr = copy(head);
-        dummyHead.next = curr;
-        
-        while(head != null) {
-
-            curr.next = copy(head.next);            // updating next pointer
-            curr.random = copy(head.random);        // updating random pointer
-
-            head = head.next;
-            curr = curr.next;
-        
-        }
-
-        return dummyHead.next;
-
-    }
-
-    private Node copy(Node node) {
-        if(node == null) return null;
-        if(oldToNewNodes.containsKey(node)) {        // if mapping is already there
-            return oldToNewNodes.get(node);
-        }
-        Node newNode = new Node(node.val);           // if not, create new node
-        oldToNewNodes.put(node, newNode);            // and create mapping
-        return newNode;
-    }
-
-}
-
-
-
-
-
-
-
-// ******************** Iterating twice ********************
-
-class Solution {
-    
-    public Node copyRandomList(Node head) {
-        
-        Node dummyHead = new Node(-1);
-        Map<Node, Node> oldToNewNodes = new HashMap<>();        // creating map from old node to new node
+        if(head == null) return null;
 
         Node old = head;
-        Node pointer = dummyHead;
+        Node copy;
         
-        while(old != null) {
+        while(old != null) {                         // adding new nodes in between
+            copy = new Node(old.val);
+            copy.next = old.next;
+            old.next = copy;
 
-            Node copy = new Node(old.val);         
-            pointer.next = copy;                                // creating new linked list
-            
-            oldToNewNodes.put(old, copy);                       // maintaining map
-
-            pointer = copy;
-
-            old = old.next;
-        
+            old = old.next.next;
         }
 
         old = head;
-        pointer = dummyHead.next;
 
-        while(old != null) {
-            
-            pointer.random = oldToNewNodes.get(old.random);     // get random of old node and get mapping of it from map to get new random node
+        while(old != null) {                         // setting up random pointers for new nodes
+            if(old.random != null) 
+                old.next.random = old.random.next;
+            old = old.next.next;
+        }
+
+        old = head;
+        copy = head.next;
+        Node ans = copy;
+
+        while(copy.next != null) {                   // removing unnecessary pointers and get two seperate lists
+
+            old.next = old.next.next;
+            copy.next = copy.next.next;
 
             old = old.next;
-            pointer = pointer.next;
-
+            copy = copy.next;
         }
-        
-        return dummyHead.next;
+        old.next = old.next.next;
+
+        return ans;
 
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+// // Time Complexity:  O(n)
+// // Space Complexity: O(n)
+
+
+
+// // ******************** Iterating once ********************
+
+// class Solution {
+
+//     Map<Node, Node> oldToNewNodes;
+    
+//     public Node copyRandomList(Node head) {
+        
+//         Node dummyHead = new Node(-1);
+//         oldToNewNodes = new HashMap<>();            // creating map from old node to new node
+
+//         Node curr = copy(head);
+//         dummyHead.next = curr;
+        
+//         while(head != null) {
+
+//             curr.next = copy(head.next);            // updating next pointer
+//             curr.random = copy(head.random);        // updating random pointer
+
+//             head = head.next;
+//             curr = curr.next;
+        
+//         }
+
+//         return dummyHead.next;
+
+//     }
+
+//     private Node copy(Node node) {
+//         if(node == null) return null;
+//         if(oldToNewNodes.containsKey(node)) {        // if mapping is already there
+//             return oldToNewNodes.get(node);
+//         }
+//         Node newNode = new Node(node.val);           // if not, create new node
+//         oldToNewNodes.put(node, newNode);            // and create mapping
+//         return newNode;
+//     }
+
+// }
+
+
+
+
+
+
+
+// // ******************** Iterating twice ********************
+
+// class Solution {
+    
+//     public Node copyRandomList(Node head) {
+        
+//         Node dummyHead = new Node(-1);
+//         Map<Node, Node> oldToNewNodes = new HashMap<>();        // creating map from old node to new node
+
+//         Node old = head;
+//         Node pointer = dummyHead;
+        
+//         while(old != null) {
+
+//             Node copy = new Node(old.val);         
+//             pointer.next = copy;                                // creating new linked list
+            
+//             oldToNewNodes.put(old, copy);                       // maintaining map
+
+//             pointer = copy;
+
+//             old = old.next;
+        
+//         }
+
+//         old = head;
+//         pointer = dummyHead.next;
+
+//         while(old != null) {
+            
+//             pointer.random = oldToNewNodes.get(old.random);     // get random of old node and get mapping of it from map to get new random node
+
+//             old = old.next;
+//             pointer = pointer.next;
+
+//         }
+        
+//         return dummyHead.next;
+
+//     }
+
+// }
