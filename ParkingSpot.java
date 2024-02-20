fbb0bf98b71e26cc7574166e4ccdaa819d857d2d
Kalyan Kumar Kancharla <kalyankancharla457@gmail.com>
2024-02-19 21:34:41 -0600

    Completed Design-5

---
+++ ParkingSpot.java
@@ -0,0 +1,25 @@
+public class ParkingSpot {
+    int floor;
+    int spot;
+
+    public ParkingSpot(int floor, int spot) {
+        this.floor = floor;
+        this.spot = spot;
+    }
+
+    public int getFloor() {
+        return floor;
+    }
+
+    public void setFloor(int floor) {
+        this.floor = floor;
+    }
+
+    public int getSpot() {
+        return spot;
+    }
+
+    public void setSpot(int spot) {
+        this.spot = spot;
+    }
+}
